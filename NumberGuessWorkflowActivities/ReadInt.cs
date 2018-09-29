30f7e133d696fa7286e7af1fa02b8f11552d3e63
Breno Batista Machado <brenomachado@gmail.com>
2018-09-29 22:08:56 +0800

    Implementing the Microsoft tutorial Get Started

---
+++ NumberGuessWorkflowActivities/ReadInt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace NumberGuessWorkflowActivities
+{
+
+    public sealed class ReadInt : NativeActivity<int>
+    {
+        [RequiredArgument]
+        public InArgument<string> BookmarkName { get; set; }
+
+        protected override bool CanInduceIdle => true;
+
+        // Se a sua atividade retornar um valor, derive de CodeActivity<TResult>
+        //e retorne o valor do método Execute.
+        protected override void Execute(NativeActivityContext context)
+        {
+            string name = BookmarkName.Get(context);
+
+            if (name == string.Empty)
+            {
+                throw new ArgumentException("BookmarkName cannot be an Empty string.",
+                    "BookmarkName");
+            }
+
+            context.CreateBookmark(name, new BookmarkCallback(OnReadComplete));
+        }
+
+        private void OnReadComplete(NativeActivityContext context, Bookmark bookmark, object state)
+        {
+            this.Result.Set(context, Convert.ToInt32(state));
+        }
+    }
+}
---
+++ NumberGuessWorkflowHost/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NumberGuessWorkflowHost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.Run(new WorkflowHostForm());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            //AutoResetEvent syncEvent = new AutoResetEvent(false);
+            //AutoResetEvent idleEvent = new AutoResetEvent(false);
+
+            //var inputs = new Dictionary<string, object>() { { "MaxNumber", 100 } };
+
+            //WorkflowApplication wfApp =
+            //    new WorkflowApplication(new StateMachineNumberGuessWorkflow(), inputs)
+            //    {
+            //        Completed = delegate(WorkflowApplicationCompletedEventArgs e)
+            //        {
+            //            int Turns = Convert.ToInt32(e.Outputs["Turns"]);
+            //            Console.WriteLine($"Congratulations, you guessed the number in {Turns} turns.");
+
+            //            syncEvent.Set(); 
+            //        },
+
+            //        Aborted = delegate (WorkflowApplicationAbortedEventArgs e)
+            //        {
+            //            Console.WriteLine(e.Reason);
+            //            syncEvent.Set();
+            //        },
+
+            //        OnUnhandledException = delegate (WorkflowApplicationUnhandledExceptionEventArgs e)
+            //        {
+            //            Console.WriteLine(e.UnhandledException.ToString());
+            //            return UnhandledExceptionAction.Terminate;
+            //        },
+
+            //        Idle = delegate (WorkflowApplicationIdleEventArgs e)
+            //        {
+            //            idleEvent.Set();
+            //        }
+            //    };
+
+            //wfApp.Run();
+
+            //WaitHandle[] handles = new WaitHandle[] { syncEvent, idleEvent };
+            //while (WaitHandle.WaitAny(handles) != 0)
+            //{
+            //    // Gather the user input and resume the bookmark.
+            //    bool validEntry = false;
+            //    while (!validEntry)
+            //    {
+            //        if (!Int32.TryParse(Console.ReadLine(), out int Guess))
+            //        {
+            //            Console.WriteLine("Please enter an integer.");
+            //        }
+            //        else
+            //        {
+            //            validEntry = true;
+            //            wfApp.ResumeBookmark("EnterGuess", Guess);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
